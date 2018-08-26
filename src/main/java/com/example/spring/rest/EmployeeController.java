98b41ac41674641c3458fe61d2dbdbdd439af862
Andrey <andrey.slesarchuk@gmail.com>
2018-08-26 16:33:31 +0300

    first step

---
+++ src/main/java/com/example/spring/rest/EmployeeController.java
@@ -0,0 +1,42 @@
+package com.example.spring.rest;
+
+import com.example.spring.model.Employee;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+
+@RequestMapping("employees")
+
+public class EmployeeController {
+
+    Employee employee = new Employee();
+
+    @RequestMapping(value = "/{name}", method = RequestMethod.GET, produces = "application/json")
+
+    public @ResponseBody Employee getEmployeeInJSON(@PathVariable String name) {
+
+        employee.setName(name);
+
+        employee.setEmail("employee1@genuitec.com");
+
+        return employee;
+
+    }
+
+    @RequestMapping(value = "/{name}.xml", method = RequestMethod.GET, produces = "application/xml")
+
+    public @ResponseBody Employee getEmployeeInXML(@PathVariable String name) {
+
+        employee.setName(name);
+
+        employee.setEmail("employee1@genuitec.com");
+
+        return employee;
+
+    }
+
+}
